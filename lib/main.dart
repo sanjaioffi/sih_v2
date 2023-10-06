9e1184dee72ad80ab66b45a3d6f07b9574ce4121
Mitun Suresh <mitunpokkisham@gmail.com>
2023-10-06 21:47:32 +0530

    conflict fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sih_v2/features/ui/location_selector/location_selector.dart';
 import 'package:sih_v2/firebase_options.dart';
 import 'package:sih_v2/test.dart';
 
